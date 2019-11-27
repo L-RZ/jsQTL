0d702587d7ac7ec14360c993f412a17f0eea6c1a
L-RZ <45250925+L-RZ@users.noreply.github.com>
2019-11-27 17:15:18 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+# running on ubuntu:16.04
+
+# self scritp dependence
+# lib: htseq pandas numpy scipy matplotlib
+
+apt-get update && \
+apt-get install --yes \
+python-pip && \
+pip install htseq pandas numpy scipy matplotlib
+
+
